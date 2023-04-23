ae1f58e9b9462ecdf835a3a2f9364586d5e457d8
kerokero014 <kevinjrm.kr@gmail.com>
2023-04-22 23:03:16 -0600

    almost

--- scripts/date.js
+++ scripts/date.js
@@ -1,9 +1,9 @@
 
 const today = new Date(document.lastModified);
 const Annee = today.getFullYear();
-const hueres = ('0' + mySuperDuperDate.getHours()).slice(-2);
-const minutes = ('0' + mySuperDuperDate.getMinutes()).slice(-2);
-const secondes = ('0' + mySuperDuperDate.getSeconds()).slice(-2);
+const hueres = ('0' + today.getHours()).slice(-2);
+const minutes = ('0' + today.getMinutes()).slice(-2);
+const secondes = ('0' + today.getSeconds()).slice(-2);
 
 document.getElementById('copyrightYear').innerHTML = Annee
 document.getElementById('lastUpdatedDate').innerHTML = `${today.getDate()}/${today.getMonth()}/${today.getFullYear()} ${hueres}:${minutes}:${secondes}`
