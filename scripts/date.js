0db12575a56782895eb5e52ca2ccaccd31979f42
kerokero014 <kevinjrm.kr@gmail.com>
2023-04-22 22:46:04 -0600

    physc

--- scripts/date.js
+++ scripts/date.js
@@ -1,8 +1,9 @@
 
-const today = new Date();
-const year = today.getFullYear();
-const msg = `&copy;  ${year} | Kevin J. Mendoza | Rexburg, ID <br/>
-<b>Last Updated: </b>${document.lastModified}`;
+const today = new Date(document.lastModified);
+const Annee = today.getFullYear();
+const hueres = ('0' + mySuperDuperDate.getHours()).slice(-2);
+const minutes = ('0' + mySuperDuperDate.getMinutes()).slice(-2);
+const secondes = ('0' + mySuperDuperDate.getSeconds()).slice(-2);
 
-const el = document.querySelector("footer");
-el.innerHTML = msg;
+document.getElementById('copyrightYear').innerHTML = Annee
+document.getElementById('lastUpdatedDate').innerHTML = `${today.getDate()}/${today.getMonth()}/${today.getFullYear()} ${hueres}:${minutes}:${secondes}`
