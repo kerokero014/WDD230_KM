c5d509a2b28b70c71983d17745361c15649c2c62
kerokero014 <kevinjrm.kr@gmail.com>
2023-05-25 17:19:58 -0600

    updates

--- chambers/scripts/windChill.js
+++ chambers/scripts/windChill.js
@@ -4,3 +4,24 @@ var windChill= (35.74 + (0.6215 * temp))-(35.75 * Math.pow(wSpeed,0.16)) + (0.42
 
 var windChill= Math.round(windChill);
 document.getElementById("windChill").innerHTML= windChill;
+
+///comments 
+////var temp = document.getElementById('temp');
+////var wind = document.getElementById('windSpeed');
+////var degree = temp.textContent;
+////var temperature = Number(degree);
+////var windS = wind.textContent;
+////var windSpeed = Number(windS); 
+////
+////  
+////if (temperature <= 50 && windSpeed > 3.0) {
+////    var windChill = calculateWindChillFactor(temperature, windSpeed);
+////    document.getElementById('result').textContent = windChill;
+////} else {
+////    document.getElementById('result').textContent = 'N/A';
+////}
+////  
+////  function calculateWindChillFactor(temperature, windSpeed) {
+////    var windChill = 35.74 + 0.6215 * temperature - 35.75 * Math.pow(windSpeed, 0.16) + 0.4275 * temperature * Math.pow(windSpeed, 0.16);
+////    return windChill.toFixed(0);
+////  }
