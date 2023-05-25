a3d26dd7cf3f221842ae5424bc52d9c06188368a
kerokero014 <kevinjrm.kr@gmail.com>
2023-05-25 17:41:40 -0600

    jscript done

--- chambers/scripts/windChill.js
+++ chambers/scripts/windChill.js
@@ -3,7 +3,14 @@ var wSpeed= 4;
 var windChill= (35.74 + (0.6215 * temp))-(35.75 * Math.pow(wSpeed,0.16)) + (0.4275*temp*Math.pow(wSpeed,0.16));
 
 var windChill= Math.round(windChill);
-document.getElementById("windChill").innerHTML= windChill;
+
+if (temp <= 50 && wSpeed > 3){
+    document.getElementById("windChill").innerHTML= windChill;
+}
+else{ 
+    document.getElementById("windChill").textContent =  "N/A"
+}
+
 
 ///comments 
 ////var temp = document.getElementById('temp');
