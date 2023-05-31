50ba5631f4641941fac22f987aa530b0842890db
kerokero014 <kevinjrm.kr@gmail.com>
2023-05-31 17:19:43 -0600

    updated banner

--- chambers/scripts/date.js
+++ chambers/scripts/date.js
@@ -7,3 +7,16 @@ const secondes = ('0' + today.getSeconds()).slice(-2);
 
 document.getElementById('copyrightYear').innerHTML = Annee
 document.getElementById('lastUpdatedDate').innerHTML = `${today.getDate()}/${today.getMonth()}/${today.getFullYear()} ${hueres}:${minutes}:${secondes}`
+
+
+/// -----------------BANNER --------------///
+
+const d = new Date();
+const day = d.getDay();
+
+if (day == 1 || day == 2) {
+    const banner = document.querySelector("#banner");
+    banner.textContent = "Come join us for the Chamber every Wedsnday @ 6:00 P.M.";
+    banner.style.display = "block";
+}
+
