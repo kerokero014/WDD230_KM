1dc9c68c528ab9a486ea8f65eda94c4d6eb66ca5
kerokero014 <kevinjrm.kr@gmail.com>
2023-06-17 17:32:32 -0600

    fixed

--- chambers/scripts/busCards.js
+++ chambers/scripts/busCards.js
@@ -1,5 +1,4 @@
-const url =
-  "https://kerokero014.github.io/WDD230_KM/chambers/scripts/data.json";
+const url = "https://kerokero014.github.io/WDD230_KM/chambers/scripts/data.json";
 
 async function getCompaniesData() {
   const response = await fetch(url);
@@ -13,7 +12,7 @@ getCompaniesData();
 const displayStore = (stores) => {
   const cards = document.querySelector("div#cards");
 
-  stores.forEach((stores) => {
+  stores.forEach((store) => {
     let card = document.createElement("section");
     let logourl = document.createElement("img");
     let address = document.createElement("p");
@@ -21,22 +20,22 @@ const displayStore = (stores) => {
     let weburl = document.createElement("a");
     let h2 = document.createElement("h2");
 
-    h2.textContent = `${stores.companyName}`;
-    address.textContent = `${stores.compLocation}`;
-    phone.textContent = `${stores.phone}`;
-    weburl.textContent = `${stores.weburl}`;
+    h2.textContent = `${store.companyName}`;
+    address.textContent = `${store.compLocation}`;
+    phone.textContent = `${store.phone}`;
+    weburl.textContent = `${store.weburl}`;
 
-    logourl.setAttribute("src", stores.logourl);
-    logourl.setAttribute("alt", `${stores.companyName} logo`);
+    logourl.setAttribute("src", store.logourl);
+    logourl.setAttribute("alt", `${store.companyName} logo`);
     logourl.setAttribute("loading", "lazy");
 
     card.appendChild(h2);
     card.appendChild(logourl);
-    card.appendChild(compLocation);
+    card.appendChild(address);
     card.appendChild(phone);
 
     cards.appendChild(card);
-  })
+  });
 };
 
 const gridbutton = document.querySelector("#grid-button");
