b498d24070d6181bf7645a16ba2985fc4a6bb54f
kerokero014 <kevinjrm.kr@gmail.com>
2023-06-28 18:33:06 -0600

    wht for?

--- chambers/scripts/date.js
+++
@@ -1,8 +0,0 @@
-const today = new Date(document.lastModified);
-const year = today.getFullYear();
-const hours = ('0' + today.getHours()).slice(-2);
-const minutes = ('0' + today.getMinutes()).slice(-2);
-const seconds = ('0' + today.getSeconds()).slice(-2);
-
-document.getElementById('yearPlaceholder').textContent = year;
-document.getElementById('lastUpdatedPlaceholder').textContent = `${today.toLocaleDateString()} ${hours}:${minutes}:${seconds}`;
