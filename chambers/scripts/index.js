dcb4073f3ec3ffa62e4a9ae4c80bc1bc83fb4b67
kerokero014 <kevinjrm.kr@gmail.com>
2023-07-01 12:36:15 -0600

    updated

--- chambers/scripts/index.js
+++ chambers/scripts/index.js
@@ -1,10 +1,26 @@
-// Get the current date and time
-const currentDate = new Date();
-const currentTime = currentDate.toLocaleTimeString();
+// Function to format the date and time
+function formatDateTime(dateTime) {
+    const options = {
+      year: 'numeric',
+      month: 'long',
+      day: 'numeric',
+      hour: 'numeric',
+      minute: 'numeric',
+      second: 'numeric',
+      hour12: true
+    };
+    return dateTime.toLocaleString(undefined, options);
+  }
+  
+  // Get the current date and time
+  const currentDate = new Date();
+  const formattedDateTime = formatDateTime(currentDate);
+  
+  // Get the current year
+  const currentYear = currentDate.getFullYear();
+  
+  // Update the HTML elements with the formatted date and current year
+  document.getElementById('currentDateTime').textContent = formattedDateTime;
+  document.getElementById('currentYear').textContent = currentYear;
 
-// Get the last updated date
-const lastUpdatedDate = document.lastModified;
-
-// Update the elements with the current time and last updated date
-document.getElementById('lastUpdatedDate').textContent = lastUpdatedDate;
-document.getElementById('currentTime').textContent = currentTime;
+  
